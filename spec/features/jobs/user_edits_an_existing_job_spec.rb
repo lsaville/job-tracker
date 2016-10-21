3b191ad283173df419c224a47e1849a979e6b07c
Lee Saville <l.w.saville@gmail.com>
2016-10-21 11:06:05 -0600

    Add edit functionality to jobs

---
+++ spec/features/jobs/user_edits_an_existing_job_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "User edits an existing job" do
+  scenario "a user can edit a job" do
+    company = Company.create(name: "ESPN", city: "LA")
+    jobs = company.jobs << Job.create(title: "schmuck", description: "haciendose la vaca", level_of_interest: 2000 )
+    job = jobs.first
+
+    visit company_path(company, job.id)
+    click_link "schmuck"
+    expect(page).to have_current_path(company_job_path(company, job))
+    click_link "Edit"
+    expect(page).to have_current_path(edit_company_job_path(company, job))
+    fill_in "job_title", with: "Awesome Job Title"
+    click_button "Update Job"
+    expect(page).to have_content "Awesome Job Title"
+  end
+end
