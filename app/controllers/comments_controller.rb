cd3e629aa2cdf4a454f919644bf6a8a3db0a2041
Lee Saville <l.w.saville@gmail.com>
2016-10-22 17:54:31 -0600

    Start implementation of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,14 @@
 class CommentsController < ApplicationController
-  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.job_id = params[:job_id]
+    @comment.save
+    redirect_to 
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
