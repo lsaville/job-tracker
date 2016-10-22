352a5dd16e6bc8015c22bd83c00dd8bc2ac0a944
Lee Saville <l.w.saville@gmail.com>
2016-10-22 12:13:26 -0600

    Add category_id to jobs to reflect job/category relationship in db

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,9 +35,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def delete
-  end
-
   private
 
   def category_params
---
+++ spec/features/categories/user_deletes_a_category_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "User deletes a category" do
+  scenario "they click a button and get redirected to " do 
+
+  end
+end
