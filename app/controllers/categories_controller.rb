56d7fbe3cef7b72b231612acced7b80307c503cc
Lee Saville <l.w.saville@gmail.com>
2016-10-22 12:21:07 -0600

    Add flash notice for creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
+      flash[:success] = "#{@category.title} created!"
       redirect_to category_path(@category)
     else
       render :new
