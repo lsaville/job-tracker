74861544b51abb268b87ce0e9c7e730bcd7c1eac
Lee Saville <l.w.saville@gmail.com>
2016-10-23 14:28:46 -0600

    Add ordered self method to clean companies controller

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,8 @@ class Company < ActiveRecord::Base
   validates :name, uniqueness: true
   has_many :jobs, dependent: :destroy
   has_many :contacts, dependent: :destroy
+
+  def self.ordered
+    order(:city)
+  end
 end
