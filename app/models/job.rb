9b153959a011f1ab43f43701247e120e59c63347
Lee Saville <l.w.saville@gmail.com>
2016-10-23 14:36:10 -0600

    Add method to get count of jobs by level of interest

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,8 @@ class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :category
   has_many :comments
+
+  def self.ordered_level_of_interest
+    group(:level_of_interest).order(level_of_interest: :desc).count
+  end
 end
