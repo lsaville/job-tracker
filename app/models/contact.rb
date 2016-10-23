64b0b782566a3a41a0cb6c5cb7cc3947bc94e2a0
Lee Saville <l.w.saville@gmail.com>
2016-10-23 07:54:40 -0600

    Complete validation testing and model for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,8 @@
 class Contact < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :postion, presence: true
+  validates :position, presence: true
   validates :email, presence: true
-  validates :company, presence: true
 
   belongs_to :company
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   resources :companies do
     resources :jobs
   end
 
   resources :categories, except: [:delete]
   resources :comments, only: [:create]
+  resources :contacts, only: [:create]
 end
