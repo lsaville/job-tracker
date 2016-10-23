2a2b57f66fb1a933cc5952882a4a0f2d9f74a89d
Lee Saville <l.w.saville@gmail.com>
2016-10-23 07:33:04 -0600

    Add new files for contact

---
+++ app/models/contact.rb
@@ -0,0 +1,9 @@
+class Contact < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :postion, presence: true
+  validates :email, presence: true
+  validates :company, presence: true
+
+  belongs_to :company
+end
