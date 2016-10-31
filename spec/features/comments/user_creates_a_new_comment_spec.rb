8988ca4957ddcab2b5ea44f4909cc4fa57452c41
Lee Saville <l.w.saville@gmail.com>
2016-10-31 10:01:44 -0600

    Refactor job to relational chaining

--- spec/features/comments/user_creates_a_new_comment_spec.rb
+++ spec/features/comments/user_creates_a_new_comment_spec.rb
@@ -4,8 +4,7 @@ describe "user creates a comment" do
   scenario "they visit a job page, enter info, submit and are shown the same job page with the comment" do
     company = Company.create(name: "ESPN", city: "LA")
     category = Category.create(title: "Blah")
-    jobs = company.jobs << Job.create(title: "schmuck", description: "haciendose la vaca", level_of_interest: 2000, category: category )
-    job = jobs.first
+    job = company.jobs.create(title: "schmuck", description: "haciendose la vaca", level_of_interest: 2000, category: category )
 
     visit company_job_path(company, job)
     fill_in "comment_content", with: "Had drinks with the ceo, we share a passion for goldfish, seems promising"
